ea3e65557d39e43fcbb247249080cb78517477db
catacernis <real.lxm@gmail.com>
2022-01-05 01:03:33 +0800

    updated nvim configurations

---
+++ nvim.arch/nvim/lua/simon/telescope.lua
@@ -0,0 +1,9 @@
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
+keymap("n", "<leader>fm", ":Telescope marks<CR>", opts)
