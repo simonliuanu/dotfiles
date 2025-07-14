061c2641eb0a75aec4361989b8b8b5e4eeb52635
Simon <ximeng.liu@anu.edu.au>
2025-07-15 03:51:05 +1000

    Update nvm config to suit different os

--- zim/zsh/nvm.zsh
+++ zim/zsh/nvm.zsh
@@ -1,4 +1,14 @@
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
 
+case "$(uname -s)" in
+  Linux)
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+    ;;
+  Darwin)
+    NVM_HBREW="/opt/homebrew/opt/nvm"
+    [ -s "$NVM_HBREW/nvm.sh" ] && . "$NVM_HBREW/nvm.sh"
+    [ -s "$NVM_HBREW/etc/bash_completion.d/nvm" ] && \
+      . "$NVM_HBREW/etc/bash_completion.d/nvm"
+    ;;
+esac
