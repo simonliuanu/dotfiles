e92ef9e480cfffa86787b6093f6b1bdd4c917600
melo <m310pup@gmail.com>
2026-08-16 16:18:01 +0800

    Update system options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.g.neovide then
+  local sysname = vim.uv.os_uname().sysname
+
+  if sysname == "Darwin" then
+  elseif sysname == "Linux" then
+    vim.o.guifont = "Maple Mono NF CN:h12"
+    vim.cmd("cd /home/melopup/Projects/")
+  end
+end
